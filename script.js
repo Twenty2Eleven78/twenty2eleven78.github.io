1c1413be6453245ca40915b6dd4b71a79c8a748b
Twenty2Eleven78 <mark@e-missive.co.uk>
2024-11-17 16:16:23 +0000

    Update V3.0.2

--- script.js
+++ script.js
@@ -28,11 +28,9 @@ function startStopwatch() {
 
 function formatTime(seconds) {
   const hours = Math.floor(seconds / 3600);
-  const minutes = Math.floor((seconds % 3600)   
- / 60);
+  const minutes = Math.floor((seconds % 3600) / 60);
   const secs = seconds % 60;
-  return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${secs.toString().padStart(2,   
- '0')}`;
+  return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${secs.toString().padStart(2,'0')}`;
 }
 
 startPauseButton.addEventListener('click', startStopwatch);
